aa33f884c0a01413ecf6fb200ec4394e981139ac
r3d_io <aanuj111@gmail.com>
2019-04-21 11:51:44 +0530

    fitness function added

--- chromosome.py
+++ chromosome.py
@@ -23,20 +23,35 @@ def intial_population(population_size, num_queens):
 		for gene in range(num_queens):
 			new_ind = Chromosome(num_queens)
 			individiual.append(new_ind)
+		individiual = fitness(individiual)
 		population.append(individiual)
 	return population
+
+def fitness(individiual):
+	conflicts = 0
+	for i in range(len(individiual)):
+		for j in range(i):
+			if (individiual[i].x == individiual[j].x):
+				conflicts += 1
+			if (individiual[i].y == individiual[j].y):
+				conflicts += 1
+			if ( abs(individiual[i].x - individiual[j].x)	== abs(individiual[i].y - individiual[j].y) ):
+				conflicts += 1
+	individiual.append(conflicts)
+	return individiual
+	#print(individiual[j], end=" ")
 
 def print_population(population):
 	for solution in population:
 		for individiual_solution in solution:
 			print(str(individiual_solution), end = ' ')
-		print() 	
+		print()
 
 def main():
-	population_size = 10 
+	population_size = 10
 	num_queens = 5
 	population = intial_population(population_size, num_queens)
-	print_population(population)		
+	#print_population(population)
 
 if __name__ == '__main__':
-		main()	
+		main()
