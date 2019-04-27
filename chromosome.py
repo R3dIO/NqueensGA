59f044cba19202cfefd9d49ad5c277aad61c1cb6
r3d_io <aanuj111@gmail.com>
2019-04-27 11:48:45 +0530

    updated mutation function

--- chromosome.py
+++ chromosome.py
@@ -17,6 +17,7 @@ class Chromosome:
 		self.r = n + self.x - self.y
 
 	def __str__(self):
+		return ("[X: {self.x}, Y: {self.y}]".format(self=self))
 		return ("[(X: {self.x} Y: {self.y}),(L: {self.l} R: {self.r})]".format(self = self))
 
 def print_population(population):
@@ -33,7 +34,7 @@ def print_matrix(population, num_queens):
 				board[gene.x-1][gene.y-1] = 'X'
 		print(np.matrix(board))
 		board = [[0 for x in range(num_queens)] for y in range(num_queens)]
-	print()	
+	print()
 
 def intial_population(population_size, num_queens):
 	population = []
@@ -105,11 +106,16 @@ def crossover(mating_pool,num_queens,pool_size,population_size):
 		population.append(kid)
 	return	population
 
-def mutation(population, num_queens, population_size):
+def mutation(population, num_queens, population_size, mutation_rate):
+	mutant = np.argmax(list(map(itemgetter(-1), population)))
+	for gene in range(len(population[mutant])-1):
+	 for ngene in range(gene,len(population[mutant])-1):
+	 	if (population[mutant][gene].x == population[mutant][ngene].x) or (population[mutant][gene].y == population[mutant][ngene].y):
+	 		powers = gene
+	 		break
+	 	break
 	mutation_prob = random.uniform(0,1)
-	if (mutation_prob < 0.1):
-		mutant = random.randint(0,population_size-1)
-		powers = random.randint(0,num_queens-1)
+	if (mutation_prob < mutation_rate):
 		individual = population[mutant][powers]
 		individual.x,individual.y = individual.y,individual.x
 		return True
@@ -126,20 +132,24 @@ def main():
 	num_queens = int(input("Enter the number of queens : ") or "5")
 	population_size = int(input("Enter the population size : ") or "12")
 	pool_size = int(input("Enter the mating pool size : ") or "6")
-	generation_num = int(input("Enter the total number of Generations : ") or "10")
+	mutation_rate = float(input("Enter the mutation rate : ") or "0.1")
+	num_generation = int(input("Enter the total number of Generations : ") or "10")
 	#Generate & print initial population
 	population = intial_population(population_size, num_queens)
-	print('population');print_population(population);
+	print('population');print_population(population);print_matrix(population,num_queens)
 	#Generate & print next population
-	for index in range( generation_num ):
+	for index in range( num_generation ):
+		#genetic operations
 		mating_pool = selection(population, pool_size)
 		next_gen = assign_fitness(crossover(mating_pool,num_queens,pool_size,population_size))
-
-		print("Mutation status {}".format(mutation(population,num_queens,population_size)))
-		print('\nmating pool');print_population(mating_pool);print('\nnext gen');print_population(next_gen)
+		mutation_status = mutation(population,num_queens,population_size,mutation_rate)
+		#Output to console
 		print("Generation number :",index+1)
+		print('\nmating pool');print_population(mating_pool)
+		print("Mutation status {}".format(mutation_status))
+		print('\nnext gen');print_population(next_gen)
 		print_matrix(next_gen, num_queens)
-
+		#convergence check
 		if convergence(next_gen): break
 		population = next_gen
 
